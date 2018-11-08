5d554b80e210afab71823d77a6f074f7c2a27808
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-11-09 00:25:58 +0700

    fix quiz overview in manage quiz

--- app/module/mod_quiz/formEditQuiz.php
+++ app/module/mod_quiz/formEditQuiz.php
@@ -1,8 +1,8 @@
 <?php
 
 $id=$_GET["id"];
-$query="SELECT * FROM tb_quiz WHERE id=$id";
-$result=mysqli_query($con,$query);
+$sql="SELECT * FROM tb_quiz WHERE id=$id";
+$result=mysqli_query($con,$sql);
 
 $quiz='';
 if(mysqli_num_rows($result)==1){
@@ -20,53 +20,78 @@ if(mysqli_num_rows($result)==1){
             <article>
                 <h3 class="article-title">Edit Quiz <?php echo $quiz["nama"]; ?></h3>
                     <div class="accordion" id="accordionExample">
-                    <div class="card">
-                        <div class="card-header" id="headingOne">
-                        <h5 class="mb-0">
-                            <button class="btn btn-block btn-link text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Collapsible Group Item #1
-                            </button>
-                        </h5>
-                        </div>
 
-                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                            <div class="card-footer text-muted text-center">
-                                Edit
+                    <?php
+                    $query = "SELECT * FROM tb_soal WHERE quiz_id=$id";
+                    $result = mysqli_query($con, $query);
+
+                    if (mysqli_num_rows($result) > 0){
+                        $index = 1;
+                        while($row = mysqli_fetch_assoc($result)){
+                            $soal_id = $row["id"];
+
+
+                            ?>
+                            <div class="card">
+                                <div class="card-header" id="heading<?php echo $index; ?>">
+                                    <h5 class="mb-0">
+                                        <div class="col-md-12">
+                                            <div class="row ">
+                                                <div class="col">
+                                                    <div class="row">
+                                                        <button class="btn btn-block btn-link text-left" type="button" data-toggle="collapse" data-target="#collapse<?php echo $index; ?>" aria-expanded="true" aria-controls="collapse<?php echo $index; ?>">
+                                                        Soal <?php echo $index;?>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-auto">
+                                                    <div class="row">
+                                                        <a href='route.php?id=<?php echo $id ?>&module=editPlayer' class='btn btn-warning'><i class='fa fa-pencil'></i></a>
+                                                        <a href='' class='btn btn-dark'><i class='fa fa-ban'></i></a>
+                                                        <a href='' class='btn btn-danger'><i class='fa fa-trash'></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </h5>
+                                </div>
+                                <div id="collapse<?php echo $index; ?>" class="collapse" aria-labelledby="heading<?php echo $index; ?>" data-parent="#accordionExample">
+                                    <div class="card-body">
+                                        <?php echo $row["soal"]; ?>                                        
+                                    </div>
+                                    <div class="card-body">
+
+                                    <?php
+                                    $query_pilihan = "SELECT * FROM tb_pilihan WHERE soal_id=$soal_id";
+                                    $result_pilihan = mysqli_query($con, $query_pilihan);
+
+                                    if (mysqli_num_rows($result_pilihan) > 0){
+                                        while($row_pilihan = mysqli_fetch_assoc($result_pilihan)){
+
+                                            ?>
+                                            <div class="alert <?php echo ($row_pilihan["status"] == 'benar') ? 'alert-warning' : 'alert-danger'; ?>" role="alert">
+                                                <?php echo $row_pilihan["pilihan"]?>
+                                            </div>
+                                            <?php 
+                                        }
+                                    }
+                                    ?>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-header" id="headingTwo">
-                        <h5 class="mb-0">
-                            <button class="btn btn-block btn-link text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Collapsible Group Item #2
-                            </button>
-                        </h5>
-                        </div>
-                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-block btn-link text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Collapsible Group Item #3
-                            </button>
-                        </h5>
-                        </div>
-                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                            <?php 
+                            $index++;
+                        }
+                    }
+                    // close mysql connection
+                    mysqli_close($con); 
+                    ?>
+
+
+
+                        
+                        
+                    
                 
                 <!-- <form action="module/mod_player/aksiPlayer.php" method="POST" enctype="multipart/form-data">
                     <fieldset>
