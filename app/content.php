f24a445bff7d2bbc8232d9d9758fbcab543043fd
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-12-11 18:50:02 +0700

    add permission & page not found

--- app/content.php
+++ app/content.php
@@ -1,46 +1,62 @@
 <?php
 // error_reporting(0);
-// session_start();
+session_start();
 // include "../koneksi/koneksi.php";
 // include "../fungsi/library.php";
 // include "../fungsi/class_paging.php";
 // include "../fungsi/fungsi_indotgl.php";
 // include "../fungsi/fungsi_combobox.php";
 
-// Bagian Home
-if ($_GET['module']=='home'){
-	include "module/mod_beranda/beranda.php";
+
+
+if ($_SESSION['level'] == 'admin') 
+{
+	// Bagian home
+	if ($_GET['module']=='home'){
+		include "module/mod_beranda/beranda.php";
+	}
+	
+	// Bagian Player
+	elseif ($_GET['module']=='player'){
+		include "module/mod_player/player.php";
+	}
+	elseif ($_GET['module']=='editPlayer'){
+		include "module/mod_player/formEditPlayer.php";
+	}
+	
+	// Bagian Admin
+	elseif ($_GET['module']=='admin'){
+		include "module/mod_admin/admin.php";
+	}
+	elseif ($_GET['module']=='editAdmin'){
+		include "module/mod_admin/formEditAdmin.php";
+	}
+	
+	// Bagian Quiz
+	elseif ($_GET['module']=='quiz'){
+		include "module/mod_quiz/quiz.php";
+	}
+	elseif ($_GET['module']=='editQuiz'){
+		include "module/mod_quiz/formEditQuiz.php";
+	}
+	elseif ($_GET['module']=='editSoal'){
+		include "module/mod_quiz/formEditSoal.php";
+	}
+	
+	// Apabila modul tidak ditemukan
+	else{
+	//   echo "<p><b>MODUL BELUM ADA ATAU BELUM LENGKAP</b></p>";
+	  include "module/page_not_found.php";
+	}
+}
+else {
+	// Bagian Home
+	if ($_GET['module']=='home'){
+		include "module/mod_beranda/beranda.php";
+	}
+	else{
+		include "module/page_not_found.php";
+	}
 }
 
-// Bagian Player
-elseif ($_GET['module']=='player'){
-	include "module/mod_player/player.php";
-}
-elseif ($_GET['module']=='editPlayer'){
-	include "module/mod_player/formEditPlayer.php";
-}
-
-// Bagian Admin
-elseif ($_GET['module']=='admin'){
-	include "module/mod_admin/admin.php";
-}
-elseif ($_GET['module']=='editAdmin'){
-	include "module/mod_admin/formEditAdmin.php";
-}
-
-// Bagian Quiz
-elseif ($_GET['module']=='quiz'){
-	include "module/mod_quiz/quiz.php";
-}
-elseif ($_GET['module']=='editQuiz'){
-	include "module/mod_quiz/formEditQuiz.php";
-}
-elseif ($_GET['module']=='editSoal'){
-	include "module/mod_quiz/formEditSoal.php";
-}
-
-// Apabila modul tidak ditemukan
-else{
-  echo "<p><b>MODUL BELUM ADA ATAU BELUM LENGKAP</b></p>";
-}
 ?>
---
+++ app/module/page_not_found.php
@@ -0,0 +1,13 @@
+<main class="container-fluid">
+    <div class="row">
+
+        <!-- Main content -->
+        <div class="col-md-12">
+            <header class="d-none d-md-block">
+                <h1><span>Sorry :(</span></h1>
+                <h5>Page not found</h5>
+            </header>
+        </div>
+
+    </div>
+</main>
