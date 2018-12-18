004e3cadebda607448af056072a14a907813de41
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-12-18 10:12:51 +0700

    fix banned user while access after login

--- app/route.php
+++ app/route.php
@@ -9,14 +9,23 @@ if (empty($_SESSION['username']))
 }
 else
 {
+    $id_user = $_SESSION["id_user"];
+    $query = "SELECT terblokir FROM tb_user WHERE id = $id_user";
+    $row = mysqli_fetch_assoc(mysqli_query($con, $query));
 
-    if ($_SESSION['level'] == 'admin') 
-    {
-        include 'admin.php';
-    }
-    else if ($_SESSION['level'] == 'player') 
-    {
-        include 'player.php';
+    if ($row["terblokir"] == 'true') {
+        session_destroy();
+        $error = urlencode("Akun anda terblokir");
+        header("Location: ../index.php?pesan=$error");
+    } else {
+        if ($_SESSION['level'] == 'admin') 
+        {
+            include 'admin.php';
+        }
+        else if ($_SESSION['level'] == 'player') 
+        {
+            include 'player.php';
+        }
     }
 }
 
