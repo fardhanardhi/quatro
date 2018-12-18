dd0c74c293bbf654912f9129d89dfca8b302db19
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-12-18 10:49:41 +0700

    remove footer

--- app/admin.php
+++ app/admin.php
@@ -109,68 +109,6 @@
             <!-- include content -->
             <?php include "content.php"; ?>
 
-            <!-- Footer -->
-            <div class="container-fluid footer-container">
-                <footer class="footer">
-                    <div class="footer-lists">
-                        <div class="row">
-                            <div class="col-sm">
-                                <ul>
-                                    <li>
-                                        <h4>Proin accumsan</h4>
-                                    </li>
-                                    <li><a href="#">Rutrum nulla a ultrices</a></li>
-                                    <li><a href="#">Blandit elementum</a></li>
-                                    <li><a href="#">Proin placerat accumsan</a></li>
-                                    <li><a href="#">Morbi hendrerit libero </a></li>
-                                    <li><a href="#">Curabitur sit amet tellus</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm">
-                                <ul>
-                                    <li>
-                                        <h4>Condimentum</h4>
-                                    </li>
-                                    <li><a href="#">Curabitur sit amet tellus</a></li>
-                                    <li><a href="#">Morbi hendrerit libero </a></li>
-                                    <li><a href="#">Proin placerat accumsan</a></li>
-                                    <li><a href="#">Rutrum nulla a ultrices</a></li>
-                                    <li><a href="#">Cras dictum</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm">
-                                <ul>
-                                    <li>
-                                        <h4>Suspendisse</h4>
-                                    </li>
-                                    <li><a href="#">Morbi hendrerit libero </a></li>
-                                    <li><a href="#">Proin placerat accumsan</a></li>
-                                    <li><a href="#">Rutrum nulla a ultrices</a></li>
-                                    <li><a href="#">Curabitur sit amet tellus</a></li>
-                                    <li><a href="#">Donec in ligula nisl.</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm">
-                                <h4>Suspendisse</h4>
-                                <p>Integer mattis blandit turpis, quis rutrum est. Maecenas quis arcu vel felis
-                                    lobortis iaculis fringilla at ligula. Nunc dignissim porttitor dolor eget porta.</p>
-
-                                <p class="social-icons">
-                                    <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
-                                    <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
-                                    <a href="#"><i class="fa fa-youtube fa-2x"></i></a>
-                                    <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="footer-bottom">
-                        <p class="text-center">Made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://github.com/fardhanardhi">fardhanardhi</a>.</p>
-                        <p class="text-center"><a href="#">Back to top</a></p>
-                    </div>
-                </footer>
-            </div>
         </div>
     </div>
 
