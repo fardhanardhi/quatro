4294c6b3c323bc872de58c2e36057ee1e2ebe8fd
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-12-20 18:40:52 +0700

    update info quiz

--- app/module/mod_play/infoQuiz.php
+++ app/module/mod_play/infoQuiz.php
@@ -1,7 +1,23 @@
 <?php
 
 $id=$_GET["id"];
-$query="SELECT * FROM tb_quiz WHERE id=$id";
+$query = "
+    SELECT
+        `tb_user`.`nama` AS `penyusun`,
+        `tb_quiz`.`id`,
+        `tb_quiz`.`nama`,
+        `tb_quiz`.`waktu`,
+        `tb_quiz`.`status`,
+        `tb_quiz`.`kode`
+    FROM
+        `tb_user`
+    RIGHT JOIN
+        `tb_quiz`
+    ON
+        `tb_user`.`id` = `tb_quiz`.`user_id`
+    WHERE `tb_quiz`.`id` = $id
+";
+
 $result=mysqli_query($con,$query);
 
 $quiz='';
@@ -10,6 +26,16 @@ if(mysqli_num_rows($result)==1){
 }else{
     echo "Quiz tidak ditemukan";
 }
+
+function minutes($time){
+    $time = explode(':', $time);
+    return ($time[0]*60) + ($time[1]) + ($time[2]/60);
+}
+
+$minutesConvert = minutes($quiz["waktu"]);
+
+$sql = "SELECT * FROM tb_soal WHERE quiz_id = $id";
+$jml_soal = mysqli_query($con, $sql);
 ?>
 
 
@@ -28,11 +54,49 @@ if(mysqli_num_rows($result)==1){
                     <fieldset>
                         <legend>Info Quiz</legend>
                         <input type="hidden" name="idQuiz" value="<?php echo $id ?>">
-
-                        <div class="form-group">
-                            <input type="text" name="kode" placeholder="Masukan kode Quiz"></input>
-                            <button type="submit" name="mulai" class="btn btn-primary">Mulai</button>
-                        </div>
+                        <table class="table">
+                            <tr>
+                                <td>
+                                    Nama Quiz:
+                                </td>
+                                <td>
+                                    <?php echo $quiz["nama"]?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nama Penyusun:
+                                </td>
+                                <td>
+                                    <?php echo $quiz["penyusun"]?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Waktu Pengerjaan:
+                                </td>
+                                <td>
+                                    <?php echo $minutesConvert?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Jumlah Soal
+                                </td>
+                                <td>
+                                    <?php echo mysqli_num_rows($jml_soal); ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <input class="form-control" type="text" name="kode" placeholder="Masukan kode Quiz"></input>
+                                </td>
+                                <td>
+                                    <button type="submit" name="mulai" class="btn btn-primary">Mulai</button>
+                                </td>
+                            </tr>
+                        </table>
+                            
 
                     </fieldset>
                 </form>
